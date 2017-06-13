fb0bd50c7e8d4badf12ff0cd10397be845ef0766
QuazarRDesigns <lilymatt2002@gmail.com>
2017-06-13 22:44:57 +1200

    Add more categories and carbs

--- projects/CarbCounterPlusAngular/app/carbcalc/carbcalc.js
+++ projects/CarbCounterPlusAngular/app/carbcalc/carbcalc.js
@@ -37,23 +37,34 @@ angular.module('myApp.carbcalc', ['ngRoute'])
                   {name: 'Multi-Grain Bread', carb: 43, amounts: [{key: '1 regular slice', value: 0.26}, {key: '1 large slice', value: 0.41}, {key: ' 100g', value: 1}]},
                   {name: 'White Bread', carb: 49, amounts: [{key: "1 thin slice (crust not eaten)", value: 0.09}, {key: '1 slice (crust not eaten)', value: 0.12}, {key: '1 very thin slice', value: 0.15}, {key: '1 thin slice', value: 0.20}, {key: '1 slice', value: 0.25}, {key: '1 large slice', value: 0.30}, {key: ' 100g', value: 1}]}
                 ]},
-              {name: 'Starch', carbs: [
-                  {name: 'Rice', carb: 28, amounts: [{key: '1/2 a cup', value: 0.79}, {key: ' 100g', value: 1}, {key: '1 cup', value: 1.58}]},
-                  {name: 'Potato', carb: 17, amounts: [{key: ' 1 cup diced', value: 0.75}, {key: ' 100g', value: 1}, {key: ' 1 small', value: 1.7}, {key: '1 medium', value: 2.13}, {key: '1 large', value: 3.69}]}
+              {name: 'Breakfast Cereals', carbs: [
+                  {name: 'Oatbran', carb: 67, amounts: [{key: '1/4 cup', value: 0.30}, {key: ' 100g', value: 1}]},
+                  {name: 'Porridge, made with milk', carb: 13, amounts: [{key: '1 cup', value: 2.60}, {key: ' 100g', value: 1}]},
+                  {name: 'Porridge, made with water', carb: 8, amounts: [{key: '1 cup', value: 2.60}, {key: ' 100g', value: 1}]},
+                  {name: 'Rolled Oats, raw', carb: 56, amounts: [{key: '1/2 cup', value: 0.45}, {key: ' 100g', value: 1}]}
                 ]},
               {name: 'Fruit', carbs: [
-                  {name: 'Banana', carb: 23, amounts: [{key: ' 100g', value: 1}, {key: ' 1 small', value: 1.01}, {key: '1 medium', value: 1.18}, {key: '1 large', value: 1.36}, {key: '1 extra large', value: 1.52}]},
                   {name: 'Apple', carb: 14, amounts: [{key: ' 100g', value: 1}, {key: '1 extra small', value: 1.01}, {key: '1 cup slices', value: 1.09}, {key: '1 cup chopped or quartered', value: 1.25}, {key: '1 small', value: 1.49}, {key: '1 medium', value: 1.82}, {key: '1 large', value: 2.23}]},
+                  {name: 'Avacado', carb: 9, amounts: [{key: ' 100g', value: 1}, {key: '1 cup sliced', value: 1.46}, {key: '1 cup cubes', value: 1.50}, {key: '1 Whole', value: 2.01}, {key: '1 cup pureed', value: 2.3}]},
+                  {name: 'Banana', carb: 23, amounts: [{key: ' 100g', value: 1}, {key: ' 1 small', value: 1.01}, {key: '1 medium', value: 1.18}, {key: '1 large', value: 1.36}, {key: '1 extra large', value: 1.52}]},
                   {name: 'Kiwifruit', carb: 15, amounts: [{key: "1 whole (5cm dia)", value: 0.69}, {key: ' 100g', value: 1}, {key: '1 cup sliced', value: 1.8}]},
-                  {name: 'Mandarin', carb: 13, amounts: [{key: '1 small', value: 0.76}, {key: '1 medium', value: 0.88}, {key: ' 100g', value: 1}, {key: '1 large', value: 1.2}]},
-                  {name: 'Avacado', carb: 9, amounts: [{key: ' 100g', value: 1}, {key: '1 cup sliced', value: 1.46}, {key: '1 cup cubes', value: 1.50}, {key: '1 Whole', value: 2.01}, {key: '1 cup pureed', value: 2.3}]}
+                  {name: 'Mandarin', carb: 13, amounts: [{key: '1 small', value: 0.76}, {key: '1 medium', value: 0.88}, {key: ' 100g', value: 1}, {key: '1 large', value: 1.2}]}
+                ]},
+              {name: 'Grains', carbs: [
+                  {name: 'Basmati Rice (Cooked)', variant: 'Rice', carb: 25.22, amounts: [{key: '1/2 cup', value: 0.79}, {key: ' 100g', value: 1}, {key: '1 cup', value: 1.58}]}
+                ]},
+              {name: 'Vegetables', carbs: [
+                  {name: 'Beetroot, Fresh, canned, drained, sliced', carb: 10, amounts: [{key: '1 Slice', value: 0.30}, {key: ' 100g', value: 1}]},
+                  {name: 'Broad Beans', carb: 8.8, amounts: [{key: ' 100g', value: 1},{key: '1 cup', value: 1.70}]},
+                  {name: 'Carrots, Sliced, boiled, drained', carb: 5.7, amounts: [{key: ' 100g', value: 1},{key: '1 cup', value: 1.57}]},
+                  {name: 'Potato', carb: 17, amounts: [{key: ' 1 cup diced', value: 0.75}, {key: ' 100g', value: 1}, {key: ' 1 small', value: 1.7}, {key: '1 medium', value: 2.13}, {key: '1 large', value: 3.69}]}
                 ]}
             ];
 
             $scope.list = [];
 
             $scope.amounts = [];
-            
+
             $scope.carbs = [];
 
             $scope.result = 0;
@@ -65,7 +76,6 @@ angular.module('myApp.carbcalc', ['ngRoute'])
                 sum = Math.round(sum + add);
               }
               $scope.result = Math.round(sum / $scope.carbUnit.value * 10) / 10;
-              console.log($scope.list);
             };
 
             $scope.getCarbs = function () {
@@ -117,7 +127,7 @@ angular.module('myApp.carbcalc', ['ngRoute'])
                   value: 1
                 };
                 temp.carb = $scope.customcarb;
-                temp.name = $scope.customcarbname ? $scope.customcarbname :'Custom Entry';
+                temp.name = $scope.customcarbname ? $scope.customcarbname : 'Custom Entry';
                 $scope.list.push(temp);
                 temp = "";
                 $scope.updateResult();
